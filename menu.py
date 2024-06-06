a7bac2717362f51910d539c631335ce1947af893
Felipe Flores <fe.floresm@duocuc.cl>
2024-06-06 09:21:29 -0400

    Agregar el menu llamado "meny.py"

---
+++ menu.py
@@ -0,0 +1,21 @@
+import time
+def menu():
+    while True:
+        print("1.- Comprar entradas")
+        print("2.- Comprar comida")
+        print("3.- Canjear cupon")
+        print("4.- Salir")
+        op = int(input("Ingrese su opción -> "))
+        if op == 1:
+            return entradas()
+        elif op == 2:
+            return comida()
+        elif op == 3:
+            return cupon()
+        elif op == 4:
+            print("Cerrando Sesión...")
+            time.sleep(.5)
+            break
+        else:
+            print("La opción ingresada no es valida.")
+
