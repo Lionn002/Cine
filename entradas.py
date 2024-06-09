00b4401533474e92139726e22044a443b386ca32
Felipe Flores <fe.floresm@duocuc.cl>
2024-06-09 17:40:27 -0400

    Se agregan las selccion de tipos de entradas y empezar a crear matrices para mostrar los asientos

--- entradas.py
+++ entradas.py
@@ -1,4 +1,20 @@
-def entradas():
-    print("¿Cuantas entradas desea comprar?")
-    r = int(input("Ingrese su respuesta -> "))  
+import butacas as bu
+def entradas(): 
+    while True:
+        print("Tipos de entradas:")
+        print("1.- Entrada 2D - Butaca Tradicional")
+        print("VALOR 5700 + IVA")
+        print("2.- Entrada 2D - Butaca Dbox")
+        print("VALOR 9200 + IVA")
+        print("0.- Volver al menu")
+        O = int(input("Seleccione su opción -> "))
+        if O == 1 or O == 2:
+            print("¿Cuantas entradas desea comprar?")
+            R = int(input("Ingrese su respuesta -> "))
+            return R
+        elif O == 0:
+            return None
+        else: 
+            print("Ignrese una opción valida.")
+def gEntradas():
     
