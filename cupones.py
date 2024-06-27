e7f77b937c96d00977b285222238db95dc1ee240
Felipe F <163019974+Lionn002@users.noreply.github.com>
2024-06-27 09:29:36 -0400

    ajustamso el codigo para aplicar un descuento aleatorio entre 10-25%

--- cupones.py
+++ cupones.py
@@ -1,21 +1,11 @@
 import rutt
 from datetime import datetime, timedelta
+import random
 
 cupon_uso = {}
 
 def porcentaje_por_rut(rut):
-    ultimo_digito = rut[-1]
-    if ultimo_digito.isdigit():
-        ultimo_digito = int(ultimo_digito)
-        if ultimo_digito == 0:
-            return 7
-        elif ultimo_digito % 2 == 0:
-            return 6
-        else:
-            return 5
-    elif ultimo_digito.lower() == 'k':
-        return 7
-    return 0
+    return random.randint(10, 25)
 
 def tiempo_de_cupon(rut):
     ahora = datetime.now()
@@ -26,19 +16,12 @@ def tiempo_de_cupon(rut):
     cupon_uso[rut] = ahora
     return True
 
-
-def aplicar_descuentos():
+def aplicar_descuentos(rut):
     while True:
         print("1.- Aplicar descuento")
         print("2.- Salir")
         opcion = int(input("Ingrese una opción: "))
         if opcion == 1:
-            rut = input("Ingrese el RUT (No ingrese el -) (o escriba 'salir'): ")
-            if rut.lower() == 'salir':
-                break
-            if not rutt.validar_rut(rut):
-                print("Error: El RUT ingresado no es válido. Intente nuevamente.")
-                continue
             if not rutt.rut_existe(rut):
                 print("Error: El RUT ingresado no existe. Debe registrarse primero.")
                 continue
@@ -47,7 +30,7 @@ def aplicar_descuentos():
                 continue
             descuento = porcentaje_por_rut(rut)
             print(f"Descuento aplicado: {descuento}%")
-
+            rutt.usuarios[rut]["descuento"] = descuento
         elif opcion == 2:
             print("Adios")
             break
