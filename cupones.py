ede5268b1c37a3c6113bca1ffd2b085db0fc5e8f
Gabriel C <166561588+gabrielcaroca@users.noreply.github.com>
2024-06-26 23:25:25 -0400

    Update cupones.py

--- cupones.py
+++ cupones.py
@@ -1,10 +1,56 @@
+import rutt
+from datetime import datetime, timedelta
 
-#que el cupon sea aleatorio con minimo de 10 letras y numeros(no pueden ser solamente letras o unicamente numeros(tiene que tener minimo una letra y un numero))
+cupon_uso = {}
 
-#la parte del codigo linea 5 quizas no se tenga que utilizar ya que tiene que ser aleatorio o almenos que quiera que sea especifico el codigo del cupon
+def porcentaje_por_rut(rut):
+    ultimo_digito = rut[-1]
+    if ultimo_digito.isdigit():
+        ultimo_digito = int(ultimo_digito)
+        if ultimo_digito == 0:
+            return 7
+        elif ultimo_digito % 2 == 0:
+            return 6
+        else:
+            return 5
+    elif ultimo_digito.lower() == 'k':
+        return 7
+    return 0
 
-#entre 5 a 30% de descuento puede obtener de los cupones para pagar sus peliculas y comida (tienen que ser diferentes tipos de cupones)
-#los cupones tienen limite 1 por dia en la compra total (por persona)
-#tendre que importar el inicio de secion de la persona y validar si utilizo un cupon  en 24 horas y si utilizo un cupon, este le diga que no puede utilizar otro cupon 
+def tiempo_de_cupon(rut):
+    ahora = datetime.now()
+    if rut in cupon_uso:
+        tiempo_desde_ultimo_uso = ahora - cupon_uso[rut]
+        if tiempo_desde_ultimo_uso < timedelta(hours=24):
+            return False
+    cupon_uso[rut] = ahora
+    return True
 
 
+def aplicar_descuentos():
+    while True:
+        print("1.- Aplicar descuento")
+        print("2.- Salir")
+        opcion = int(input("Ingrese una opción: "))
+        if opcion == 1:
+            rut = input("Ingrese el RUT (No ingrese el -) (o escriba 'salir'): ")
+            if rut.lower() == 'salir':
+                break
+            if not rutt.validar_rut(rut):
+                print("Error: El RUT ingresado no es válido. Intente nuevamente.")
+                continue
+            if not rutt.rut_existe(rut):
+                print("Error: El RUT ingresado no existe. Debe registrarse primero.")
+                continue
+            if not tiempo_de_cupon(rut):
+                print("No puede usar otro cupón dentro de 24 horas.")
+                continue
+            descuento = porcentaje_por_rut(rut)
+            print(f"Descuento aplicado: {descuento}%")
+
+        elif opcion == 2:
+            print("Adios")
+            break
+
+if __name__ == "__main__":
+    aplicar_descuentos()
