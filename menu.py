c529c058b8a7482cd03346331ba3db95f6b8040f
Felipe F <163019974+Lionn002@users.noreply.github.com>
2024-06-27 09:26:35 -0400

    Se ajusta el menu para validar con los nuevos parametros

--- menu.py
+++ menu.py
@@ -1,32 +1,40 @@
 import time
+import entradas as en
+import comida as co
+import cupones as cu
+import rutt
 
-def menu():
-    import entradas as en
-    import comida as co
-    import cupones as cu
+def main_menu(rut):
     while True:
+        print(f"Bienvenido {rut}")
         print("[1] Comprar entradas")
         print("[2] Comprar comida")
         print("[3] Canjear cupon")
-        print("[4] Salir")
-        op = 0
+        print("[4] Generar boleta")
+        print("[5] Salir")
         op = int(input("Ingrese su opción -> "))
         if op == 1:
-            op = 0
-            return en.entradas()
+            en.entradas(rut)
         elif op == 2:
-            op = 0
-            return co.comida()
+            co.comida(rut)
         elif op == 3:
-            op = 0
-            return cu.aplicar_descuentos()
+            cu.aplicar_descuentos(rut)
         elif op == 4:
-            op = 0
+            import boleta
+            boleta.generar_boleta(rut)
+            print("Fin del programa. Gracias por su compra.")
+            break
+        elif op == 5:
             print("Cerrando Sesión...")
             time.sleep(.5)
             break
         else:
             print("La opción ingresada no es valida.")
 
-if __name__ == "__main__":
-    menu()
+def menu():
+    rut = rutt.main()
+    if not rut:
+        return
+    main_menu(rut)
+
+menu()
